0bbe498162cd3ab78a24371b9e111fbfc5eed4d3
amin <aminbahmani10@gmail.com>
2020-01-17 16:47:00 +0330

    implement bottomNavBar

---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,63 @@
+import 'package:cryptofolio/widget/home_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Main extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MainState();
+  }
+}
+
+
+class MainState extends State<Main>{
+
+  int _selectedSheet = 0;
+  final _sheets =[
+    Home(),Home(),Home(),Home(),Home()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _sheets[_selectedSheet],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.blue,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        currentIndex: _selectedSheet,
+        onTap: (int index){
+          setState(() {
+              _selectedSheet = index;
+          });},
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,color: Colors.black,),
+            title: Text("خانه"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work,color: Colors.black),
+            title: Text("دارایی")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline,color: Colors.black),
+            title: Text("بازار جهانی")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore,color: Colors.black),
+            title: Text("اخبار")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings,color: Colors.black),
+            title: Text("تنظیمات"),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+}
---
+++ lib/widget/home_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Image.asset('images/blockchain.png'), onPressed: null),
+        title: Text("Cryptofolio"),
+        centerTitle: true,
+        brightness: Brightness.dark,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: DataSearch());
+              })
+        ],
+      ),
+    );
+  }
+
+}
+
+class DataSearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) => ListTile());
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme.copyWith(
+        primaryColor: Colors.white,
+        primaryIconTheme: theme.primaryIconTheme.copyWith(color: Colors.grey),
+        primaryColorBrightness: Brightness.light,
+        primaryTextTheme: theme.textTheme,
+        textTheme: TextTheme(title: TextStyle(fontSize: 14.0)),
+        inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            )));
+  }
+
+  @override
+  String get searchFieldLabel => 'جستجو در ارزها';
+}
