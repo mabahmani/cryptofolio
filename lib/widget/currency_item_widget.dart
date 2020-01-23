ca16681748b19960001e10d71466f948aa55489d
amin <aminbahmani10@gmail.com>
2020-01-23 11:54:29 +0330

    currency item change style

--- lib/widget/currency_item_widget.dart
+++ lib/widget/currency_item_widget.dart
@@ -26,177 +26,170 @@ class CurrencyItemState extends State<CurrencyItem> {
         children: <Widget>[
           Row(
             children: <Widget>[
-              Expanded(
-                child: Container(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          currency.rank,
-                          style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 10),
-                        ),
-                        padding: EdgeInsets.all(2),
-                      ),
-                      Container(
-                        child: currency.logoUrl.contains(".svg") ?
-                        SvgPicture.network(currency.logoUrl,
-                          width: 32,
-                          height: 32,
-                        ):
-                        Image.network(currency.logoUrl,
-                          width: 32,
-                          height: 32,),
-                        padding: EdgeInsets.all(5),
-                      ),
-                      Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              currency.name,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12
-                              ),
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Text(
-                                  currency.symbol,
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 10),
-                                ),
-
-                                double.parse(currency.priceChangePct) < 0 ?
-                                Icon(
-                                  Icons.arrow_drop_down,
-                                  color: Colors.redAccent,
-                                ) :
-                                Icon(
-                                  Icons.arrow_drop_up,
-                                  color: Colors.green,
-                                ),
-                                double.parse(currency.priceChangePct) < 0 ?
-                                Text(
-                                  currency.priceChangePct,
-                                  style: TextStyle(
-                                    color: Colors.redAccent,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10,
-                                  ),
-                                  textDirection: TextDirection.ltr,
-                                )
-                                :
-                                Text(
-                                  currency.priceChangePct,
-                                  style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10,
-                                  ),
-                                  textDirection: TextDirection.ltr,
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                        padding: EdgeInsets.all(10),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
+              Container(
                 child: Row(
                   children: <Widget>[
                     Container(
+                      child: Text(
+                        currency.rank,
+                        style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10),
+                      ),
+                      padding: EdgeInsets.all(2),
+                    ),
+                    Container(
+                      width: 48,
+                      height: 48,
+                      child: currency.logoUrl.contains(".svg") ?
+                      SvgPicture.network(currency.logoUrl,
+                        fit: BoxFit.contain,
+                      ):
+                      Image.network(currency.logoUrl,
+                        fit: BoxFit.contain,),
+                      padding: EdgeInsets.all(5),
+                    ),
+                    Container(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(bottom: 6),
-                            child: Text(
-                              "\$" + currency.price,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12
-                              ),
+                          Text(
+                            currency.name,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12
                             ),
                           ),
                           Row(
                             children: <Widget>[
-                              Container(
-                                margin: EdgeInsets.only(right: 3),
-                                child: Text(
-                                  "Bn",
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 10),
-                                ),
+                              Text(
+                                currency.symbol,
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10),
                               ),
-                              Container(
-                                margin: EdgeInsets.only(right: 3),
-                                child: Text(
-                                  "\$" + currency.marketCap,
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 10),
-                                ),
+
+                              double.parse(currency.priceChangePct) < 0 ?
+                              Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.redAccent,
+                              ) :
+                              Icon(
+                                Icons.arrow_drop_up,
+                                color: Colors.green,
                               ),
-                              Container(
-                                margin: EdgeInsets.only(right: 3),
-                                child: Text(
-                                  "MCap",
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 10),
+                              double.parse(currency.priceChangePct) < 0 ?
+                              Text(
+                                currency.priceChangePct,
+                                style: TextStyle(
+                                  color: Colors.redAccent,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
                                 ),
-                              ),
+                                textDirection: TextDirection.ltr,
+                              )
+                              :
+                              Text(
+                                currency.priceChangePct,
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                ),
+                                textDirection: TextDirection.ltr,
+                              )
                             ],
                           )
                         ],
                       ),
                       padding: EdgeInsets.all(10),
                     ),
-                    Expanded(
-                      child: Center(
-                        child: Container(
-                          child:
-                          currency.favorite ?
-                          IconButton(
-                            icon:Icon(Icons.star,
-                            color: Colors.amber,),
-                            onPressed: (){
-                              setState(() {
-                                currency.favorite = !currency.favorite;
-                              });
-                            },
-                          )
-                          :
-                          IconButton(
-                            icon:Icon(Icons.star_border,
-                            color: Colors.grey,),
-                            onPressed: (){
-                              setState(() {
-                                currency.favorite = !currency.favorite;
-                              });
-                            },
-                        ),
-                        ),
-                      ),
-                    )
                   ],
                 ),
+              ),
+              Spacer(),
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(bottom: 6),
+                          child: Text(
+                            "\$" + currency.price,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(right: 3),
+                              child: Text(
+                                "Bn",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 3),
+                              child: Text(
+                                "\$" + currency.marketCap,
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 3),
+                              child: Text(
+                                "MCap",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Container(
+                    child:
+                    currency.favorite ?
+                    IconButton(
+                      icon:Icon(Icons.star,
+                      color: Colors.amber,),
+                      onPressed: (){
+                        setState(() {
+                          currency.favorite = !currency.favorite;
+                        });
+                      },
+                    )
+                    :
+                    IconButton(
+                      icon:Icon(Icons.star_border,
+                      color: Colors.grey,),
+                      onPressed: (){
+                        setState(() {
+                          currency.favorite = !currency.favorite;
+                        });
+                      },
+                  ),
+                  )
+                ],
               )
             ],
           )
